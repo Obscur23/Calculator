6ecb0c202abcb6ed335a992f2b20ed386f621b31
Obscur23 <obscuredo@mail.ru>
2017-12-08 22:58:21 +0300

    calc in class tested ok

--- src/main/java/Calculator/Base.java
+++ src/main/java/Calculator/Base.java
@@ -14,51 +14,8 @@ public class Base {
         //Инициализируем сканер
         switch (choise) {
             case 1 :
-                boolean cycle;
-                //Инициализируем переменную цикла повторений
-                do {
-                    //Начало цикла калькулятора
-                    System.out.println("First Digit");
-                    float first = in.nextFloat();
-                    //Ввод первого после подсказки
-                    System.out.println("Second Digit");
-                    float second = in.nextFloat();
-                    //Ввод второго после подсказки
-                    System.out.println("Operation: + - * /");
-                    String oper = in.next();
-                    //Ввод действия
-                    //in.nextLine() работает некорректно, 10х Stackoverflow
-                    if (oper.equals("+")) {
-                        float result = first + second;
-                        System.out.println("Addition ");
-                        System.out.printf("%+.4f", result);
-                        //Считаем сумму
-                    }
-                    if (oper.equals("-")) {
-                        float result = first - second;
-                        System.out.println("Difference ");
-                        System.out.printf("%+.4f", result);
-                        //Считаем разность
-                    }
-                    if (oper.equals("*")) {
-                        float result = first * second;
-                        System.out.print("Multiplication ");
-                        System.out.printf("%+.4f", result);
-                        //Считаем умножение
-                    }
-                    if (oper.equals("/")) {
-                        float result = first / second;
-                        System.out.print("Division ");
-                        System.out.printf("%.4f", result);
-                        //Считаем деление
-                    }
-                    System.out.println();
-                    System.out.println("For Continue press y, for Exit any key");
-                    String answer = in.next();
-                    //Запрос продолжения
-                    cycle = (answer.equals("y"));
-                } while (cycle);
-                System.out.println("Bye!");
+                Calc calc = new Calc();
+                calc.Calulator();
                 break;
             case 2 :
                 ArrayCount arrayclass = new ArrayCount();
